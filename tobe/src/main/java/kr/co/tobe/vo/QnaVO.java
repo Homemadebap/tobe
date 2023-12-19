f1e7bec8254a9ebf4ccabe01bc0bd88edbc556f0
Park Sang Kun <hello.git.pskpsk11@naver.com>
2023-12-19 11:51:35 +0900

    merge 작업 맞춤

--- tobe/src/main/java/kr/co/tobe/vo/QnaVO.java
+++ tobe/src/main/java/kr/co/tobe/vo/QnaVO.java
@@ -8,12 +8,9 @@ import lombok.Data;
 public class QnaVO {
 
 	private int qna_no;
-	private int course_no;
 	private int member_no;
 	private String q_title;
 	private String q_content;
-	private String q_filename_org;
-	private String q_filename_real;
 	private Timestamp q_writedate;
 	private int gno;
 	private int ono;
@@ -21,4 +18,11 @@ public class QnaVO {
 	
 	private int startIdx;
 	private int page;
+	
+	public QnaVO() {
+		this.page = 1;
+	}
+	public int getStartIdx() {
+		return (page-1) * 10;
+	}
 }
