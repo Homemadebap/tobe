0da86debca0f715ace0993948fdbf0fb187cef07
Park Sang Kun <hello.git.pskpsk11@naver.com>
2023-12-20 16:20:10 +0900

    feat 문의사항

--- tobe/src/main/java/kr/co/tobe/user/customer/UserCustomerServiceImpl.java
+++ tobe/src/main/java/kr/co/tobe/user/customer/UserCustomerServiceImpl.java
@@ -4,12 +4,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
+import kr.co.tobe.vo.FaqVO;
 import kr.co.tobe.vo.NoticeVO;
 import kr.co.tobe.vo.QnaVO;
 
@@ -92,6 +90,13 @@ public class UserCustomerServiceImpl implements UserCustomerService {
         map.put("next", next);
 		return map;
 	}
+	
+	@Override
+	public NoticeVO noticeDetail(NoticeVO vo) {
+		NoticeVO data = mapper.detail(vo.getNotice_no());
+		// TODO Auto-generated method stub
+		return data;
+	}
 
 
 	@Override
@@ -99,5 +104,41 @@ public class UserCustomerServiceImpl implements UserCustomerService {
 		// TODO Auto-generated method stub
 		return 0;
 	}
+
+
+//	@Override
+//	public int noticeInsert(NoticeVO vo) {
+//		// TODO Auto-generated method stub
+//		return 0;
+//	}
+	@Override
+	public Map<String, Object> faqList(FaqVO param) {
+		int count = mapper.faqCount(param); //총개수
+        int totalPage = count / 10; //총 페이지
+        if (count % 10 > 0) totalPage++;
+        System.out.println(param.getPage());
+        System.out.println(param.getStartIdx());
+        List<FaqVO> faqList = mapper.faqList(param); //목록
+        
+        Map<String, Object> map = new HashMap<>();
+        map.put("count", count);
+        map.put("totalPage", totalPage);
+        map.put("list", faqList);
+        
+        //페이징
+        int endPage = (int)(Math.ceil(param.getPage()/10.0)*10);
+        int startPage = endPage - 9;
+        if (endPage > totalPage) endPage = totalPage;
+        boolean prev = startPage > 1;
+        boolean next = endPage < totalPage;
+        map.put("endPage", endPage);
+        map.put("startPage", startPage);
+        map.put("prev", prev);
+        map.put("next", next);
+		return map;
+	}
+
+
+
 
 }
