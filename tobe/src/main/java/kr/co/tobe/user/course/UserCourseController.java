9a79c04a0721d1f4178df9434d5948c476af607f
parkmohoon <parkmohoon@DESKTOP-UISSM8I>
2023-12-29 14:19:58 +0900

    feat

--- tobe/src/main/java/kr/co/tobe/user/course/UserCourseController.java
+++ tobe/src/main/java/kr/co/tobe/user/course/UserCourseController.java
@@ -249,17 +249,23 @@ public class UserCourseController {
 
 
 	
-	@GetMapping("/user/course/userCourseAskForm.do")
-	public String getInfoByCourse(Model model, @RequestParam("course_no") int course_no) {
-		model.addAttribute("vo", service.getInfoByCourse(course_no, false)); 
-		return "user/course/userCourseAskForm";
-	}
+//		@GetMapping("/user/course/userCourseAskForm.do")
+//		public String getInfoByCourse(Model model, @RequestParam("course_no") int course_no) {
+//			model.addAttribute("vo", service.getInfoByCourse(course_no, false)); 
+//			return "user/course/userCourseAskForm";
+//		}
 	
-	@GetMapping("/user/course/userCourseDetailList.do")
-    public String getcourseDetail(Model model, @RequestParam("course_no") int course_no) {
-        CourseVO courseInfo = service.getlectureDetail(course_no);
-        model.addAttribute("courseInfo", courseInfo);
-        return "/user/course/userCourseDetailList";
-    }
+	   @GetMapping("/user/course/userCourseAskForm.do")
+	   public String getInfoByCourse( @RequestParam("infoCourse_no") String infoCourse_no,
+	                   		Model model, HttpSession sess ) {
+	      MemberVO user = (MemberVO)sess.getAttribute("loginInfo");
+	
+	      System.out.println("Received infoCourseNo: " + infoCourse_no);
+	      
+	      model.addAttribute("infoCourse_no", String.valueOf(infoCourse_no));
+	      
+	      return "/user/course/userCourseAskForm";
+	   }
+	   
 	   
 }
